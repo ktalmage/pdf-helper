0a0863007ed7e0aeb499c2b5915bd6ba33739d52
ktalmage <ktalmage85@gmail.com>
2020-05-03 14:53:53 -0400

    added route detail in pdfscontroller

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,13 +1,24 @@
 class PdfsController < ApplicationController
   def index
+    @pdfs = Pdf.all
   end
 
   def new
+    @pdf = Pdf.new
   end
 
   def create
   end
 
   def destroy
+    @pdf = Pdf.find(params[:id])
+      @pdf.destroy
+      redirect_to pdfs_path, notice:  "The PDF #{@pdf.name} has been deleted."
+   end
   end
+
+  private
+      def pdf_params
+      params.require(:pdf).permit(:name, :attachment)
+   end
 end
