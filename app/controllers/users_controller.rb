2c5634a06716efd4e288663cac3411a9aac66386
ktalmage <ktalmage85@gmail.com>
2020-05-03 12:10:19 -0400

    updated user and session create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create]
+  
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(params.require(:user).permit(:username,        
-      :password))
+    @user = User.create(user_params)
       session[:user_id] = @user.id
       redirect_to '/welcome'
+  end
+
+  def user_params
+    params.require(:user).permit(:email,:password,:name)
   end
 end
