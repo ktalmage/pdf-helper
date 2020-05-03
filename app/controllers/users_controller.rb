a48d3787238b92c289a811dadc2456b4ca9ecdb2
ktalmage <ktalmage85@gmail.com>
2020-05-03 11:30:19 -0400

    added create method and helper methods for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(params.require(:user).permit(:username,        
+      :password))
+      session[:user_id] = @user.id
+      redirect_to '/welcome'
   end
 end
