cfeaf2964ea2538317f48ffba3f0360c56d14b4b
ktalmage <ktalmage85@gmail.com>
2020-05-04 10:28:47 -0400

    removed additional end from Pdf model

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,5 +1,4 @@
 class Pdf < ApplicationRecord
     has_one_attached :pdf
-   validates :name, presence: true # Make sure the owner's name is present.
-end
+    validates :name, presence: true # Make sure the owner's name is present.
 end
