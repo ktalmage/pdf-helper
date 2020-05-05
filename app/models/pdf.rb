9363357657762809b68f7b649229795d413dca5a
ktalmage <ktalmage85@gmail.com>
2020-05-05 11:38:27 -0400

    redoing models, changing model relationships

--- app/models/pdf.rb
+++
@@ -1,6 +0,0 @@
-class Pdf < ApplicationRecord
-    belongs_to :user
-    
-    validates :name, presence: true # Make sure the owner's name is present.
-    
-end
--- db/migrate/20200504200903_add_user_to_pdfs.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToPdfs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :pdfs, :user_id, :integer
-  end
-end
