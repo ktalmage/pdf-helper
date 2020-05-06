540043e60155120458b6da7157ac061874347180
ktalmage <ktalmage85@gmail.com>
2020-05-06 10:49:56 -0400

    added more user validation into new user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create!(user_params)
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
       session[:user_id] = @user.id
       redirect_to '/welcome'
+    else
+      render :new
+    end
   end
 
  
