2c958736222d2472d56ccd8dd5d3e1298433cbc4
ktalmage <ktalmage85@gmail.com>
2020-05-06 10:28:28 -0400

    adjusted user validation on email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.create!(user_params)
       session[:user_id] = @user.id
       redirect_to '/welcome'
   end
