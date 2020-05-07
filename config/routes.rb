b17ffaa26dd3976f44ac128f33bd3f3c8f612635
ktalmage <ktalmage85@gmail.com>
2020-05-07 15:37:51 -0400

    undo main page route

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   resources :investments
 
-  get '/', to: 'main#home'
+ 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
