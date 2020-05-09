a8a341b60cacff691468e75694ebdf63e2cd0a91
ktalmage <ktalmage85@gmail.com>
2020-05-09 13:14:13 -0400

    email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_secure_password
     validates_presence_of :email, :password, :name
     validates :email, uniqueness: true
-    validates :password, confirmation: true, unless: -> { password.blank? }
+    validates :password, presence: true
+    validates :password_confirmation, presence: true, unless: -> { password.blank? }
     has_many :clients, through: :investments
 end
