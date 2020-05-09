dd220a672c0ace8121efe3bd30abcbf3040443a2
ktalmage <ktalmage85@gmail.com>
2020-05-09 19:02:05 -0400

    added nested new resource, added omniauth gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resources :clients do
-    resources :investments, only: [:show, :index]
+    resources :investments, only: [:show, :new, :index]
   end
  
   resources :users, only: [:show,:new, :create ]
