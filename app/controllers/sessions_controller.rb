163daaad9e314de6a9b17fca6d7c582f1dd0921f
ktalmage <ktalmage85@gmail.com>
2020-05-09 12:34:37 -0400

    adjusted show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 skip_before_action :authorized, only: [:new, :create, :welcome]
 
   def new
+    @user = User.new
   end
 
   def create
@@ -10,8 +11,8 @@ skip_before_action :authorized, only: [:new, :create, :welcome]
       session[:user_id] = @user.id
      redirect_to '/welcome'
    else
-      redirect_to '/login' 
-   end
+    redirect_to '/login' 
+  end
   end
 
   def login
