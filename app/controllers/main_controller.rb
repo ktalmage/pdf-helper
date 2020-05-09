39eaab1cea07af054fb58d627ce48772171975fd
ktalmage <ktalmage85@gmail.com>
2020-05-09 10:53:15 -0400

    main controller adjusted, index page for clients, welcome page link to clients changed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,5 @@
 class MainController < ApplicationController
     def home
-        if logged_in?
-            flash[:alert]= "You are logged in, please either logout or return to welcome page"
-            redirect_to :welcome
-        else
-            redirect_to 'main#home'
-        end
+        
     end
 end
