f506b2c55ce41c36f4e34ccbaf1d7591342952e8
ktalmage <ktalmage85@gmail.com>
2020-05-17 11:54:50 -0400

    added nested form for investments in clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -63,6 +63,6 @@ class ClientsController < ApplicationController
   end
 
   def client_params
-    params.require(:client).permit(:name,:ein)
+    params.require(:client).permit(:name,:ein,investment_attributes: [:name,:ein,:ordinary_income,:interest_income,:st_capital,:mt_capital,:lt_capital])
   end
 end
