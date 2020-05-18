3b65d7b76a0fbfc7206aa0b4424f02c44c4a2a32
ktalmage <ktalmage85@gmail.com>
2020-05-18 14:11:40 -0400

    adjusted relationship with user and client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
     validates :name, uniqueness: true
     validates :ein, uniqueness: true
     has_many :investments
-    has_and_belongs_to_many :users, through: :investments
+    has_many :users, through: :investments
     accepts_nested_attributes_for :investments
 
 end
